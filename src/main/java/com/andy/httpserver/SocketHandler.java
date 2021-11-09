af1c5b101c0750b278406b561156a5cc1b4ccd28
Andy Smith <asmith@8thlight.com>
2021-11-09 16:58:19 +0000

    Make sure we close server connection

--- src/main/java/com/andy/httpserver/SocketHandler.java
+++ src/main/java/com/andy/httpserver/SocketHandler.java
@@ -2,9 +2,14 @@ package com.andy.httpserver;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketHandler {
     public static ServerSocket createServer(int portNumber) throws IOException {
         return new ServerSocket(portNumber);
+    }
+
+    public static Socket createClient(ServerSocket server) {
+        return null;
     }
 }
--- src/test/java/com/andy/httpserver/SocketHandlerShould.java
+++ src/test/java/com/andy/httpserver/SocketHandlerShould.java
@@ -1,18 +1,37 @@
 package com.andy.httpserver;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SocketHandlerShould {
 
+    private ServerSocket server;
+
+    @AfterEach
+    void tearDown() throws IOException {
+        server.close();
+    }
+
     @Test
     void create_a_server_socket() throws IOException {
-        ServerSocket server = SocketHandler.createServer(8888);
+        server = SocketHandler.createServer(8888);
         assertNotNull(server);
     }
+
+    @Test
+    @Disabled
+    void open_a_client_socket() throws IOException {
+        ServerSocket server = SocketHandler.createServer(8888);
+        Socket client = SocketHandler.createClient(server);
+        assertNotNull(client);
+    }
+
 
 }
