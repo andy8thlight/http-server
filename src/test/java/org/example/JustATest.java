bc1f6934178ca675460895245af53dd99798cae3
Andy Smith <asmith@8thlight.com>
2021-11-09 11:11:29 +0000

    Try a refactor to see if it triggers build

--- src/test/java/org/example/JustATest.java
+++ src/test/java/org/example/JustATest.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class JustATest {
     @Test
     void ofGitHubActions() {
-        assertEquals(4, 2 + 2);
+        assertEquals(4, addUp(2, 2));
+    }
+
+    private int addUp(int a, int b) {
+        return a + b;
     }
 }
