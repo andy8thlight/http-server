e901e1a204b40ae526b3a5cd594c9ea965c3eb32
Andy Smith <asmith@8thlight.com>
2021-11-11 09:59:23 +0000

    Remove unneeded tests

--- src/test/java/com/andy/httpserver/ExampleHttpServerShould.java
+++ src/test/java/com/andy/httpserver/ExampleHttpServerShould.java
@@ -90,44 +90,12 @@ public class ExampleHttpServerShould {
 
     @Test
     void read_from_input_stream() throws IOException {
-        ByteArrayInputStream inputStream = createInputStream("some data coming in\n");
+        ByteArrayInputStream inputStream = StreamHelper.createInputStream("some data coming in\n");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         String s = bufferedReader.readLine();
 
         assertEquals("some data coming in", s);
-    }
-
-    @Test
-    @Disabled
-    void write_data_to_when_spoken_to() throws IOException {
-        ByteArrayInputStream inputStream = createInputStream("some data\n");
-        when(clientSocket.getInputStream()).thenReturn(inputStream);
-        OutputStream helloStream = new ByteArrayOutputStream();
-        when(clientSocket.getOutputStream()).thenReturn(helloStream);
-
-        exampleHttpServer.handle();
-
-        assertEquals("some data", helloStream.toString());
-    }
-
-    @Test
-    @Disabled
-    void write_more_data_to_when_spoken_to() throws IOException {
-        when(server.accept()).thenReturn(clientSocket);
-        ByteArrayInputStream inputStream = createInputStream("some data\nand some more\n");
-        when(clientSocket.getInputStream()).thenReturn(inputStream);
-        OutputStream helloStream = new ByteArrayOutputStream();
-        when(clientSocket.getOutputStream()).thenReturn(helloStream);
-
-        exampleHttpServer.handle();
-
-        assertEquals("some dataand some more", helloStream.toString());
-    }
-
-
-    ByteArrayInputStream createInputStream(String data) {
-        return new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
     }
 
 }
