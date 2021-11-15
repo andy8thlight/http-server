cc92e9bdd2db47c9c631f2cc2be4ccb715e66c2b
Andy Smith <asmith@8thlight.com>
2021-11-15 14:49:15 +0000

    Remove duplication in test

--- src/test/java/com/andy/httpserver/HttpRequestProcessorShould.java
+++ src/test/java/com/andy/httpserver/HttpRequestProcessorShould.java
@@ -1,5 +1,6 @@
 package com.andy.httpserver;
 
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -24,42 +25,32 @@ class HttpRequestProcessorShould {
     @Test
     void return_200_ok() throws IOException, BadRequestException {
         String requestData = validGetRequest("/");
-
-        ByteArrayInputStream inputStream = StreamHelper.createInputStream(requestData);
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-        httpRequestProcessor.processRequests(inputStream, outputStream);
-
+        OutputStream outputStream = processRequests(requestData);
         assertEquals("HTTP/1.1 200 OK" + CRLF + CRLF, outputStream.toString());
     }
 
     @Test
     void return_200_ok_with_body() throws IOException, BadRequestException {
         String requestData = validGetRequest("/simple_get_with_body");
-
-        ByteArrayInputStream inputStream = StreamHelper.createInputStream(requestData);
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-        httpRequestProcessor.processRequests(inputStream, outputStream);
-
+        OutputStream outputStream = processRequests(requestData);
         assertEquals("HTTP/1.1 200 OK" + CRLF + CRLF + "Hello world\n", outputStream.toString());
     }
 
     @Test
     void return_404_for_non_existant_page() throws BadRequestException, IOException {
         String requestData = validGetRequest("/non_existant");
-
-        ByteArrayInputStream inputStream = StreamHelper.createInputStream(requestData);
-        OutputStream outputStream = new ByteArrayOutputStream();
-
-        httpRequestProcessor.processRequests(inputStream, outputStream);
-
+        OutputStream outputStream = processRequests(requestData);
         assertEquals("HTTP/1.1 404 Not Found" + CRLF + CRLF, outputStream.toString());
-
     }
 
     private String validGetRequest(final String path) {
         return "GET " + path + " HTTP/1.1\nHost: localhost\n\n";
     }
 
+    private OutputStream processRequests(String requestData) throws IOException, BadRequestException {
+        ByteArrayInputStream inputStream = StreamHelper.createInputStream(requestData);
+        OutputStream outputStream = new ByteArrayOutputStream();
+        httpRequestProcessor.processRequests(inputStream, outputStream);
+        return outputStream;
+    }
 }
