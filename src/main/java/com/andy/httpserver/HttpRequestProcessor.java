0de914d97680d8d328484e0beae7590eaf53468d
asmith <asmith@8thlight.com>
2021-12-20 11:05:57 +0000

    Move route processing logic out, again

--- src/main/java/com/andy/httpserver/HttpRequestProcessor.java
+++ src/main/java/com/andy/httpserver/HttpRequestProcessor.java
@@ -14,44 +14,11 @@ public class HttpRequestProcessor implements RequestProcessor {
     public void processRequests(InputStream inputStream, OutputStream outputStream) throws IOException, BadRequestException {
         if (inputStream != null) {
             HttpRequest request = requestParser.parse(inputStream);
-            Route route = routes.getRoute(request);
-            HttpResponse response = generateResponse(request, route);
+            HttpResponse response = routes.process(request);
             sendResponse(outputStream, response);
         }
     }
 
-    private HttpResponse generateResponse(HttpRequest request, Route route) {
-        if (route == null) {
-            return new HttpResponse(HttpStatus.NOT_FOUND, "");
-        }
-
-        if (request.getMethod() != HttpMethod.OPTIONS && request.getMethod() != HttpMethod.HEAD) {
-            if (request.getMethod() != route.getHttpMethod()) {
-                HttpResponse response = new HttpResponse(HttpStatus.NOT_ALLOWED, "");
-                response.setHeader("Allow", route.getAllowHeader());
-                return response;
-            }
-        }
-
-        if (request.getMethod() == HttpMethod.OPTIONS) {
-            HttpResponse response = new HttpResponse(HttpStatus.OK, route.getBody());
-            response.setHeader("Allow", route.getAllowHeader());
-            return response;
-        }
-
-        if (request.getMethod() == HttpMethod.HEAD) {
-            return new HttpResponse(HttpStatus.OK, "");
-        }
-
-        if (request.getMethod() == HttpMethod.POST) {
-            // TODO: Special handling here
-            HttpResponse response = new HttpResponse(HttpStatus.OK, "");
-            String requestBody = request.getBody();
-            response.setBody(requestBody);
-            return response;
-        }
-        return new HttpResponse(HttpStatus.OK, route.getBody());
-    }
 
     void sendResponse(OutputStream outputStream, HttpResponse httpResponse) throws IOException {
         outputStream.write(httpResponse.toString().getBytes(StandardCharsets.UTF_8));
--- src/main/java/com/andy/httpserver/Routes.java
+++ src/main/java/com/andy/httpserver/Routes.java
@@ -10,6 +10,34 @@ public class Routes {
         this.routes.put(uri, route);
     }
 
+    public HttpResponse process(HttpRequest request) {
+        Route route = getRoute(request);
+        HttpResponse httpResponse;
+        if (route == null) {
+            httpResponse = new HttpResponse(HttpStatus.NOT_FOUND, "");
+        } else if (request.getMethod() == HttpMethod.OPTIONS) {
+            HttpResponse response = new HttpResponse(HttpStatus.OK, route.getBody());
+            response.setHeader("Allow", route.getAllowHeader());
+            httpResponse = response;
+        } else if (request.getMethod() == HttpMethod.HEAD) {
+            httpResponse = new HttpResponse(HttpStatus.OK, "");
+        } else if (request.getMethod() != route.getHttpMethod()) {
+            HttpResponse response = new HttpResponse(HttpStatus.NOT_ALLOWED, "");
+            response.setHeader("Allow", route.getAllowHeader());
+            httpResponse = response;
+        } else if (request.getMethod() == HttpMethod.POST) {
+            // TODO: Special handling here
+            HttpResponse response = new HttpResponse(HttpStatus.OK, "");
+            String requestBody = request.getBody();
+            response.setBody(requestBody);
+            httpResponse = response;
+        } else {
+            httpResponse = new HttpResponse(HttpStatus.OK, route.getBody());
+        }
+
+        return httpResponse;
+    }
+
     public Route getRoute(HttpRequest request) {
         return routes.get(request.getPath());
     }
